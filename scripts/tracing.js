eb212984914d415f6a5cf76c281a24c01f109e94
Michael Beemer <beeme1mr@users.noreply.github.com>
2022-08-23 08:34:16 -0400

    switch to jaeger exporter

--- scripts/tracing.js
+++ scripts/tracing.js
@@ -1,32 +1,33 @@
 'use strict';
 
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
-const {
-  getNodeAutoInstrumentations,
-} = require('@opentelemetry/auto-instrumentations-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 
 registerInstrumentations({
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
 const { Resource } = require('@opentelemetry/resources');
-const {
-  SemanticResourceAttributes,
-} = require('@opentelemetry/semantic-conventions');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
-const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
+
+const hostName = process.env.OTEL_TRACE_HOST || 'localhost';
+const serviceName = process.env.SERVICE_NAME || 'unknown';
+const environment = process.env.ENVIRONMENT || 'unknown';
 
 const provider = new NodeTracerProvider({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'api-service',
+    [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+    [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: environment,
   }),
 });
 
 provider.addSpanProcessor(
   new BatchSpanProcessor(
-    new ZipkinExporter({
-      url: 'http://localhost:9411/api/v2/spans',
+    new JaegerExporter({
+      endpoint: `http://${hostName}:14268/api/traces`,
     })
   )
 );
